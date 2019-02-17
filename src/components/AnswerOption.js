a0feaa7a2406c21c44c89ccfe288843f364d7b42
Ileana Argyris <argyris.ileana@gmail.com>
2019-02-18 01:30:49 +0200

    feat: Create radio buttons for options

--- src/components/AnswerOption.js
+++ src/components/AnswerOption.js
@@ -3,7 +3,18 @@ import PropTypes from 'prop-types';
 
 function AnswerOption(props) {
     return (
-
+        <li className="answerOption">
+            <input
+            type="radio"
+            className="radioButton"
+            name="radioGroup"
+            checked={props.answerType === props.answer}
+            id={props.answerType}
+            value={props.answerType}
+            disabled={props.answer}
+            onChange={props.onAnswerSelected}
+            >
+        </li>
     );
 }
 

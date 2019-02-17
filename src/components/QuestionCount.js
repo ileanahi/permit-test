92565e22d6c392c58090b39d3df63a7f784939e7
Ileana Argyris <argyris.ileana@gmail.com>
2019-02-17 22:38:27 +0200

    feat: Create QuestionCounter HTML

--- src/components/QuestionCount.js
+++ src/components/QuestionCount.js
@@ -3,8 +3,12 @@ import PropTypes from 'prop-types';
 
 function QuestionCount(props) {
     return (
-
-    )
+        <div className="questionCount">
+            Question <span>{props.counter}</span> of <span>{props.total}</span>
+        </div>
+    );
 }
+
+
 
 export default QuestionCount;

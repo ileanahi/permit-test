8ea316f4d690581a74154cf09ecefb3975029d48
Ileana Argyris <argyris.ileana@gmail.com>
2019-02-18 01:40:47 +0200

    feat: Add propTypes

--- src/components/AnswerOption.js
+++ src/components/AnswerOption.js
@@ -21,4 +21,11 @@ function AnswerOption(props) {
     );
 }
 
+AnswerOption.propTypes = {
+    answerType: PropTypes.string.isRequired,
+    answerContent: PropTypes.string.isRequired,
+    answer: PropTypes.string.isRequired,
+    onAnswerSelected: PropTypes.func.isRequired
+};
+
 export default AnswerOption;

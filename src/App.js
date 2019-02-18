77f2f78c35ee4969289d81e01464d58bd91046c2
Ileana Argyris <argyris.ileana@gmail.com>
2019-02-18 03:38:48 +0200

    feat: Add states

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import './App.css';
 import Question from './components/Question';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+      counter: 0,
+      questionId: 1,
+      question: '',
+      answerOptions: [],
+      answer: '',
+      answersCount: {
+        nintendo: 0,
+        microsoft: 0,
+        sony: 0
+      },
+      result: ''
+    };
+  }
   render() {
     return (
       <div className="App">
